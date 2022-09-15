e946b29fdda151114c8fe2ecd5158a6120fd3656
Julia Grifiths Allison <griffithsallison0615@gmail.com>
2022-09-15 12:34:42 -0400

    first commit, finished assignment(?)

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿//int answer = 4;
+//if (answer < 9)
+//{
+//    response = answer + " is less than nine";
+//}
+//else
+//{
+//   response = answer + " is greater than or equal to nine";
+//}
+
+var answer = 4;
+var message = (answer == 9) ? "Correct!" : "Sorry, I don't think that is right";
